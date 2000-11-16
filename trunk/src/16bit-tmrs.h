fa610f4dd4b4ffc5e25a82016c1b8f7b5013c8f5
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2000-11-16 12:51:14 +0000

    Added 16bit-tmrs support - timers for the 18cxxx devices

---
+++ trunk/src/16bit-tmrs.h
@@ -0,0 +1,26 @@
+/*
+   Copyright (C) 2000 T. Scott Dattalo
+
+This file is part of gpsim.
+
+gpsim is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+gpsim is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gpasm; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#ifndef __16_BIT_TMRS_H__
+#define __16_BIT_TMRS_H__
+
+#include "14bit-tmrs.h"
+
+#endif // __16_BIT_TMRS_H__
