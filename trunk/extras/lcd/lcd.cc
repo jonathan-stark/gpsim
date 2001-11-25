ce7e4279849e1749dd5d8357cc09ed872b8240b1
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2001-11-25 06:30:51 +0000

    Initial commit of LCD module

---
+++ trunk/extras/lcd/lcd.cc
@@ -0,0 +1,413 @@
+/*
+   Copyright (C) 2000 T. Scott Dattalo
+
+This file is part of gpsim.
+
+gpsim is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+gpsim is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gpsim; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+/*
+
+  lcd.cc
+
+  This is a fairly complete/complicated gpsim module that simulates
+  an LCD display. This version is currently hardcoded for the Hitachi
+  style 2 row by 20 column display. The font is also hardcoded to 5 by
+  7 pixels.
+
+  Hardware simulation:
+
+  The Hitachi displays commonly have a 14pin interface:
+
+  8 data lines
+  3 control lines (E,RS,R/W)
+  pwr
+  gnd
+  contrast
+
+  This version only supports the 8 data and 3 control lines.
+
+  Software simulation:
+
+  This software uses an event driven behavior model to simulate
+  the LCD display. This means that when one of the I/O lines toggle
+  the module will be notified and will respond to the event. A state
+  machine is used to control the behavior. 
+
+*/
+
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream.h>
+
+#include <gtk/gtk.h>
+
+#include "lcd.h"
+pic_processor *gpsim_get_active_cpu(void);
+void  gpsim_set_break_delta(guint64 delta, BreakCallBack *f=NULL);
+
+
+extern "C" {
+
+  // This function is registerd with gpsim when a module
+  // is instantiated. It gets invoke when gpsim has stopped
+  // simulating. 
+
+  static void simulation_has_stopped(gpointer lcd)
+    {
+      if(lcd)
+	{
+	  ((LcdDisplay *)lcd)->update();
+	}
+    }
+
+}
+
+
+//--------------------------------------------------------------
+// LcdPort class
+//
+// The LcdPort class is derived from the gpsim class "IOPORT".
+// As such, it inherits all of the IOPROT's functionality (like
+// tracing, stimulus interface, etc.). The LcdPort class extends
+// IOPORT by redirecting changes to the LCD state machine.
+
+Lcd_Port::Lcd_Port (unsigned int _num_iopins=8) : IOPORT(_num_iopins)
+{
+
+}
+void Lcd_Port::setbit(unsigned int bit_number, bool new_value)
+{
+
+  int bit_mask = 1<<bit_number;
+
+  if( ((bit_mask & value) != 0) ^ (new_value==1))
+    {
+      //cout << " Lcd+Port::set_bit bit changed due to a stimulus. new_value = "
+      //   << new_value <<'\n';
+      value ^= bit_mask;
+
+      assert_event();
+      trace_register_write();
+    }
+  //else cout <<  " IOPORT::set_bit bit did not change\n";
+
+}
+
+void Lcd_Port::assert_event(void)
+{
+  cout << __FUNCTION__ << " shouldn't be called\n";
+}
+
+
+DataPort::DataPort (unsigned int _num_iopins=8) : Lcd_Port(_num_iopins)
+{
+
+}
+ 
+void DataPort::update_pin_directions(unsigned int new_direction)
+{
+
+  if((new_direction ^ direction) & 1) {
+    direction = new_direction & 1;
+
+    // Change the directions of the I/O pins
+    for(int i=0; i<8; i++) {
+      if(pins[i]) {
+	pins[i]->update_direction(direction);
+
+	if(pins[i]->snode)
+		pins[i]->snode->update(0);
+      }
+    }
+  }
+}
+
+ 
+void Lcd_Port::trace_register_write(void)
+{
+
+  trace.module1(value);
+}
+
+//-----------------------------------------------------
+
+ControlPort::ControlPort (unsigned int _num_iopins=8) : Lcd_Port(_num_iopins)
+{
+  break_delta = 100000;
+}
+
+void ControlPort::assert_event(void)
+{
+  
+  lcd->advanceState(lcd->ControlEvents[value & 7].e);
+}
+
+void ControlPort::put(unsigned int new_value)
+{
+
+  unsigned int old_value = value;
+
+  Lcd_Port::put(new_value);
+
+  if( (old_value ^ value) & 0x7) 
+    assert_event();
+}
+
+void ControlPort::callback(void)
+{
+  if(lcd && lcd->debug)
+    cout << "ControlPort::callback(void)\n";
+
+  gpsim_set_break_delta(break_delta, this);
+
+}
+
+void DataPort::assert_event(void)
+{
+  lcd->advanceState(DataChange);
+}
+
+void DataPort::put(unsigned int new_value)
+{
+
+  unsigned int old_value = value;
+
+
+  Lcd_Port::put(new_value);
+
+  if( (old_value ^ value) & 0xff)
+    lcd->advanceState(DataChange);
+
+}
+
+//--------------------------------------------------------------
+// Lcd_Input
+//   This class is a minor extension of a normal IO_input. I may
+// remove it later, but for now it does serve a simple purpose.
+// Specifically, this derivation will intercept when a stimulus
+// is being changed. 
+
+void Lcd_Input::put_node_state( int new_state)
+{
+
+  int current_state = state;
+
+
+  IO_input::put_node_state(new_state);
+
+  if(current_state ^ state) {
+    //cout << "Lcd Input " << name() << " changed to new state: " << state << '\n';
+  }
+
+}
+
+void LcdDisplay::update(void)
+{
+  update(darea, w_width,w_height);
+}
+
+
+
+static gint
+cursor_event (GtkWidget          *widget,
+	      GdkEvent           *event,
+	      gpointer  *user_data)
+{
+  if ((event->type == GDK_BUTTON_PRESS) &&
+      ((event->button.button == 1) ||
+       (event->button.button == 3)))
+    {
+      return TRUE;
+    }
+
+  return FALSE;
+}
+
+//--------------------------------------------------------------
+// create_iopin_map 
+//
+//  This is where the information for the Module's package is defined.
+// Specifically, the I/O pins of the module are created.
+
+void LcdDisplay::create_iopin_map(void)
+{
+
+
+  // Create an I/O port to which the I/O pins can interface
+  //   The module I/O pins are treated in a similar manner to
+  //   the pic I/O pins. Each pin has a unique pin number that
+  //   describes it's position on the physical package. This
+  //   pin can then be logically grouped with other pins to define
+  //   an I/O port. 
+
+
+  control_port = new ControlPort(8);
+  control_port->value = 0;
+  control_port->lcd = this;
+
+  gpsim_set_break_delta(1000, control_port);
+
+  data_port = new DataPort(8);
+  data_port->value = 0;
+  data_port->lcd = this;
+
+
+  // Here, we name the port `pin'. So in gpsim, we will reference
+  //   the bit positions as U1.pin0, U1.pin1, ..., where U1 is the
+  //   name of the logic gate (which is assigned by the user and
+  //   obtained with the name() member function call).
+
+  char *pin_name = name();   // Get the name of this LCD
+  if(pin_name) {
+    data_port->new_name(pin_name);
+    control_port->new_name(pin_name);
+  }
+
+
+
+  // Define the physical package.
+  //   The Package class, which is a parent of all of the modules,
+  //   is responsible for allocating memory for the I/O pins.
+  //
+
+
+  create_pkg(14);
+
+
+  // Define the I/O pins and assign them to the package.
+  //   There are two things happening here. First, there is
+  //   a new I/O pin that is being created. For the binary 
+  //   indicator, both pins are inputs. The second thing is
+  //   that the pins are "assigned" to the package. If we
+  //   need to reference these newly created I/O pins (like
+  //   below) then we can call the member function 'get_pin'.
+
+  assign_pin(4, new Lcd_Input(control_port, 0,"DC"));  // Control
+  assign_pin(5, new Lcd_Input(control_port, 1,"RW"));
+  assign_pin(6, new Lcd_Input(control_port, 2,"E"));
+
+  assign_pin(7, new IO_bi_directional(data_port, 0,"d0"));  // Data bus
+  assign_pin(8, new IO_bi_directional(data_port, 1,"d1"));
+  assign_pin(9, new IO_bi_directional(data_port, 2,"d2"));
+  assign_pin(10, new IO_bi_directional(data_port,3,"d3"));
+  assign_pin(11, new IO_bi_directional(data_port,4,"d4"));
+  assign_pin(12, new IO_bi_directional(data_port,5,"d5"));
+  assign_pin(13, new IO_bi_directional(data_port,6,"d6"));
+  assign_pin(14, new IO_bi_directional(data_port,7,"d7"));
+
+  data_port->update_pin_directions(0);
+
+  // Create an entry in the symbol table for the new I/O pins.
+  // This is how the pins are accessed at the higher levels (like
+  // in the CLI).
+
+  for(int i =1; i<get_pin_count(); i++) {
+    IOPIN *p = Package::get_pin(i);
+    if(p)
+      symbol_table.add_stimulus(p);
+  }
+}
+
+//--------------------------------------------------------------
+// construct
+
+ExternalModule * LcdDisplay::construct(char *new_name=NULL)
+{
+
+  cout << " LCD display constructor\n";
+
+  LcdDisplay *lcdP = new LcdDisplay(2,20);
+  lcdP->new_name(new_name);
+  lcdP->create_iopin_map();
+
+
+  lcdP->set_pixel_resolution(5,7);
+  lcdP->set_crt_resolution(3,3);
+  lcdP->set_contrast(1.0);
+
+  lcdP->InitStateMachine();
+
+  return lcdP;
+
+}
+
+LcdDisplay::LcdDisplay(int aRows, int aCols, unsigned aType)
+{
+
+  if (verbose)
+     cout << "LcdDisplay constructor\n";
+  name_str = "Lcd Display";
+
+  mode_flag = _8BIT_MODE_FLAG;
+  data_latch_phase = 1;
+
+  disp_type=aType;
+  set_pixel_resolution();
+  set_crt_resolution();
+  set_contrast();
+
+  // Hard code to 2 rows and 20 columns.
+  rows = aRows;
+  cols = aCols;
+
+  // The font is created dynamically later on.
+  fontP = NULL;
+
+  // If you want to get diagnostic info, change debug to non-zero.
+  debug = 0;
+
+  interface_id = gpsim_register_interface((gpointer)this);
+
+  gpsim_register_simulation_has_stopped(interface_id, simulation_has_stopped);
+
+
+  CreateGraphics();
+
+
+}
+
+//-----------------------------------------------------------------
+// Displaytech 161A, added by Salvador E. Tropea <set@computer.org>
+// construct
+
+ExternalModule * LcdDisplayDisplaytech161A::construct(char *new_name=NULL)
+{
+
+  if (verbose)
+     cout << " LCD 161A display constructor\n";
+
+  LcdDisplayDisplaytech161A *lcdP = new LcdDisplayDisplaytech161A(2,8,TWO_ROWS_IN_ONE);
+  lcdP->new_name(new_name);
+  lcdP->create_iopin_map();
+
+  // Are these really needed? the constructor already does it.
+  //lcdP->set_pixel_resolution(5,7);
+  //lcdP->set_crt_resolution(3,3);
+  //lcdP->set_contrast(1.0);
+
+  lcdP->InitStateMachine();
+
+  return lcdP;
+
+}
+
+LcdDisplayDisplaytech161A::LcdDisplayDisplaytech161A(int aRows, int aCols,
+  unsigned aType) :
+  LcdDisplay(aRows,aCols,aType)
+{
+}
---
+++ trunk/extras/lcd/lcdengine.cc
@@ -0,0 +1,545 @@
+/*
+   Copyright (C) 2000 T. Scott Dattalo
+
+This file is part of gpsim.
+
+gpsim is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+gpsim is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gpsim; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+
+#include "lcd.h"
+
+/*
+  lcdengine.cc
+
+  This is where the behavioral simulation of the LCD is performed.
+  The LCD is simulated with a state machine. The states are defined
+
+  POWERON,
+  ST_INITIALIZED,
+  ST_COMMAND_PH0,
+  ST_DATA_PH0,
+  ST_STATUS_READ,
+  
+*/
+
+
+
+/****************************************************************
+ *
+ * Now the details for simulating the LCD 
+ */
+
+/*
+ * LCD Command "Set Display Data RAM Address" = 10000000
+ */
+
+static const int LCD_CMD_SET_DDRAM  = 0x80;
+static const int LCD_MASK_SET_DDRAM = 0x80;
+
+/*
+ * LCD Command "Set Display Character Generator RAM Address" = 01aaaaaa
+ */
+
+static const int LCD_CMD_SET_CGRAM  = 0x40;
+static const int LCD_MASK_SET_CGRAM = 0xc0;
+
+
+/*
+ * LCD Command Function Set =  001dnfxx
+ *  d = 1 for 8-bit interface or 0 for 4-bit interface
+ *  n = for 2 line displays, n=1 allows both lines to be displayed
+ *      while n=0 only allows the first.
+ * f = font size. f=1 is for 5x11 dots while f=0 is for 5x8 dots.
+ */
+
+static const int LCD_CMD_FUNC_SET  = 0x20;    // LCD Command "Function Set"
+static const int LCD_MASK_FUNC_SET = 0xe0;    // 
+static const int LCD_4bit_MODE     = 0x00;    // d=0
+static const int LCD_8bit_MODE     = 0x10;    // d=1
+static const int LCD_1_LINE        = 0x00;    // n=0
+static const int LCD_2_LINES       = 0x08;    // n=1
+static const int LCD_SMALL_FONT    = 0x00;    // f=0
+static const int LCD_LARGE_FONT    = 0x04;    // f=1
+
+/*
+ * LCD Command "Cursor Display" = 0001sdxx
+ *  s = 1 Sets cursor-move or display-shift
+ *  d = 1 Shift right 0 = shift left
+ */
+
+static const int LCD_CMD_CURSOR_DISPLAY   = 0x10;   // LCD Command "Cursor Display"
+static const int LCD_MASK_CURSOR_DISPLAY  = 0xf0;   // 
+
+/*
+ * LCD Command Display Control = 00001dcb
+ *  d = 1 turn display on or 0 to turn display off
+ *  c = 1 turn cursor on or 0 to turn cursor off
+ *  b = 1 blinking cursor or 0 non-blinking cursor
+ */
+
+static const int LCD_CMD_DISPLAY_CTRL  = 0x08;    // LCD Command "Display Control"
+static const int LCD_MASK_DISPLAY_CTRL = 0xf8;    // 
+static const int LCD_DISPLAY_OFF       = 0x00;    // d=0
+static const int LCD_DISPLAY_ON        = 0x04;    // d=1
+static const int LCD_CURSOR_OFF        = 0x00;    // c=0
+static const int LCD_CURSOR_ON         = 0x02;    // c=1
+static const int LCD_BLINK_OFF         = 0x00;    // b=0
+static const int LCD_BLINK_ON          = 0x01;    // b=1
+
+
+/*
+ * LCD Command "Entry Mode" = 000001is
+ *  i = 1 to increment or 0 to decrement the DDRAM address after each DDRAM access.
+ *  s = 1 to scroll the display in the direction specified by the i-bit when the
+ *       cursor reaches the edge of the display window.
+ */
+
+static const int LCD_CMD_ENTRY_MODE  = 0x04;    // LCD Command "Entry Mode"
+static const int LCD_MASK_ENTRY_MODE = 0xfc;    // 
+static const int LCD_DEC_CURSOR_POS  = 0x00;    // i=0
+static const int LCD_INC_CURSOR_POS  = 0x02;    // i=1
+static const int LCD_NO_SCROLL       = 0x00;    // s=0
+static const int LCD_SCROLL          = 0x01;    // s=1
+
+/*
+ * LCD Command "Cursor Home" = 0000001x
+ */
+
+static const int LCD_CMD_CURSOR_HOME   = 0x02;   // LCD Command "Cursor Home"
+static const int LCD_MASK_CURSOR_HOME  = 0xfe;   // 
+
+// LCD Command Clear Display = 00000001
+static const int LCD_CMD_CLEAR_DISPLAY  = 0x01;
+static const int LCD_MASK_CLEAR_DISPLAY = 0xff;
+
+
+
+
+//
+// PowerON
+//
+// Power has just been applied to the LCD. Now it's waiting
+// for a command to switch it into either 4 or 8 bit mode
+//
+/*
+void LcdDisplay::STPowerON( Event e)
+{
+
+  switch (e) {
+  case EWC:
+    start_command();
+    break;
+
+  default:
+    break;
+  }
+}
+*/
+
+void LcdDisplay::start_data(void)
+{
+
+  if(in_8bit_mode())
+    data_latch = data_port->value & 0xff;
+  else {
+    // 4-bit mode.
+    data_latch = ( (data_latch << 4) | ((data_port->value & 0xf0)>>4) ) & 0xff;
+    data_latch_phase ^= 1;
+  }
+
+  newState(ST_COMMAND_PH0);
+}
+
+void LcdDisplay::send_status(void)
+{
+  unsigned short status;
+  
+  status = ( cursor.row * 0x40 ) + cursor.col; // FIXME - No busy yet
+
+  if(in_8bit_mode()) {
+    data_port->put ( status );
+    data_port->update_pin_directions(0xFF);
+  } else {
+    if (data_latch_phase & 1 )
+      data_port->put ( status );
+    else
+      data_port->put ( status << 4 );
+    data_port->update_pin_directions(0xF0);
+    data_latch_phase ^= 1;
+  }
+
+  newState(ST_STATUS_READ);
+}
+
+void LcdDisplay::release_port(void)
+{
+  data_port->update_pin_directions(0);
+  newState(ST_INITIALIZED);
+}
+
+void LcdDisplay::execute_command(void)
+{
+  if(debug)
+    cout << "execute command:  ";
+
+  //
+  // Determine the command type
+  //
+
+  if( (data_latch & LCD_MASK_SET_DDRAM) ==  LCD_CMD_SET_DDRAM) {
+    if(debug)
+      cout << "LCD_CMD_SET_DDRAM\n";
+    write_ddram_address(data_latch & 0x7f);
+  }
+  else if( (data_latch & LCD_MASK_SET_CGRAM) ==  LCD_CMD_SET_CGRAM) {
+    cout << "LCD_CMD_SET_CGRAM\n";
+    cout << "NOT SUPPORTED\n";
+  }
+  else if( (data_latch & LCD_MASK_FUNC_SET) == LCD_CMD_FUNC_SET) {
+
+    if(debug)
+      cout << "LCD_CMD_FUNC_SET\n";
+
+    //
+    // Check the bits in the command
+    //
+
+    if(data_latch & LCD_8bit_MODE)
+      set_8bit_mode();
+    else
+      set_4bit_mode();
+
+    if(data_latch & LCD_2_LINES)
+      set_2line_mode();
+    else
+      set_1line_mode();
+
+    if(data_latch & LCD_LARGE_FONT)
+      set_large_font_mode();
+    else
+      set_small_font_mode();
+
+  }
+  else if( (data_latch & LCD_MASK_CURSOR_DISPLAY) ==  LCD_CMD_CURSOR_DISPLAY) {
+    cout << "LCD_CMD_CURSOR_DISPLAY\n";
+    cout << "NOT SUPPORTED\n";
+  }
+  else if( (data_latch & LCD_MASK_DISPLAY_CTRL) == LCD_CMD_DISPLAY_CTRL) {
+
+    if(debug)
+      cout << "LCD_CMD_DISPLAY_CTRL\n";
+
+    if(data_latch & LCD_DISPLAY_ON)
+      set_display_on();
+    else
+      set_display_off();
+
+    if(data_latch & LCD_CURSOR_ON)
+      set_cursor_on();
+    else
+      set_cursor_off();
+
+    if(data_latch & LCD_BLINK_ON)
+      set_blink_on();
+    else
+      set_blink_off();
+  }
+  else if( (data_latch & LCD_MASK_ENTRY_MODE) == LCD_CMD_ENTRY_MODE) {
+    cout << "LCD_CMD_ENTRY_MODE\n";
+    cout << "NOT SUPPORTED\n";
+  }
+  else if( (data_latch & LCD_MASK_CURSOR_HOME) ==  LCD_CMD_CURSOR_HOME) {
+    cout << "LCD_CMD_CURSOR_HOME\n";
+    cout << "NOT SUPPORTED\n";
+  }
+  else if( (data_latch & LCD_MASK_CLEAR_DISPLAY) == LCD_CMD_CLEAR_DISPLAY) {
+    if(debug)
+      cout << "LCD_CMD_CLEAR_DISPLAY\n";
+    clear_display();
+  }
+  else
+    cout << "UNKOWN command : 0x" << hex << data_latch << '\n';
+
+  // If the lcd is in '4-bit' mode, then this flag
+  // will tell us which four bits are being written.
+  data_latch_phase = 1;
+
+}
+
+
+//--------------------------------------------------
+//
+//   new_command
+//
+// The state machine will call `new_cmmand' when it enters
+// the eWC state.
+
+void LcdDisplay::new_command(void)
+{
+
+  if(in_8bit_mode())
+    execute_command();
+
+  else {
+    // 4-bit mode 
+    if(data_latch_phase & 1) 
+      execute_command();
+  }
+
+  newState(ST_INITIALIZED);
+}
+
+
+//--------------------------------------------------
+//
+//   new_data
+//
+
+void LcdDisplay::new_data(void)
+{
+
+  if(in_8bit_mode())
+    write_data(data_latch);
+
+  else {
+    // 4-bit mode 
+    if(data_latch_phase & 1) 
+      write_data(data_latch);
+  }
+
+  newState(ST_INITIALIZED);
+}
+
+//--------------------------------------------------
+//
+//   newState
+//
+
+void LcdDisplay::newState(State s)
+{
+  previous_state = current_state;
+  current_state = s;
+}
+
+//--------------------------------------------------
+//
+//   revertState
+//
+
+void LcdDisplay::revertState(void)
+{
+  current_state = previous_state;
+}
+
+//--------------------------------------------------
+//
+//   advanceState
+//
+
+void LcdDisplay::advanceState( Event e)
+{
+
+  last_event = e;
+  //cout << "Event:  " << getEventName(e) << '\n';
+  //cout << " from State:  " << getStateName(current_state) << '\n';
+
+  switch(current_state) {
+  case ST_INITIALIZED:
+  case POWERON:
+    if(e == EWC)
+      start_data();
+
+    if(e == EWD)
+      start_data();
+
+    if(e == ERC)
+      send_status();
+    break;
+
+  case ST_COMMAND_PH0:
+    if(e == eWC)
+      new_command();
+
+    if(e == eWD)
+      new_data();
+
+    break;
+
+  case ST_DATA_PH0:
+    if(e == eWD)
+      new_data();
+    else
+      ;
+
+    break;
+
+  case ST_STATUS_READ:
+    if(e == eRC)
+      release_port();
+    break;
+    
+  default:
+    break;
+
+  }
+
+  //cout << " to State:  " << getStateName(current_state) << '\n';
+}
+
+char * LcdDisplay::getStateName(State s)
+{
+
+  switch(s) {
+  case POWERON:
+    return "power on";
+  case ST_INITIALIZED:
+    return "initialized";
+  case ST_COMMAND_PH0:
+    return "command start";
+  case ST_DATA_PH0:
+    return "data start";
+  case ST_STATUS_READ:
+    return "reading status";
+
+  default:
+    break;
+  }
+
+
+  return "unknown state";
+}
+
+char *LcdDisplay::getEventName( Event e)
+{
+  switch(e) {
+  case  eRD:
+    return "eRD";
+  case  eRC:
+    return "eRC";
+  case  eWD:
+    return "eWD";
+  case  eWC:
+    return "eWC";
+  case  ERD:
+    return "ERD";
+  case  ERC:
+    return "ERC";
+  case  EWD:
+    return "EWD";
+  case  EWC:
+    return "EWC";
+  case  DataChange:
+    return "Data Change";
+  default:
+    break;
+  }
+
+  return "unknown state";
+}
+
+void LcdDisplay::viewInternals(int verbosity)
+{
+
+  cout << "Lcd Display: " << name() << '\n';
+  cout << "Current state: " << getStateName(current_state) << '\n';
+  cout << "Last event: " << getEventName(last_event) << '\n';
+
+  if(verbosity>1) {
+
+    cout << "  " << ( (in_8bit_mode()) ? '8' : '4') << "bit mode\n";
+    cout << "  " << ( (in_2line_mode()) ? '2' : '1') << "-line mode\n";
+    cout << "  " << ( (in_large_font_mode()) ? "large" : "small") << " font\n";
+
+  }
+
+}
+
+void LcdDisplay::InitStateMachine(void)
+{
+
+  ControlEvents[0].init(eWC, "eWC");
+  ControlEvents[1].init(eWD, "eWD");
+  ControlEvents[2].init(eRC, "eRC");
+  ControlEvents[3].init(eRD, "eRD");
+  ControlEvents[4].init(EWC, "EWC");
+  ControlEvents[5].init(EWD, "EWD");
+  ControlEvents[6].init(ERC, "ERC");
+  ControlEvents[7].init(ERD, "ERD");
+
+  current_state = POWERON;
+
+  int j;
+  
+  for(j=0; j<cols; j++)
+    ch_data[0][j] = 255;
+
+  for(j=0; j<cols; j++)
+    ch_data[1][j] = 0;
+
+  if(debug)
+    test();
+}
+
+void LcdDisplay::test(void)
+{
+
+  set_8bit_mode();
+
+  data_port->value = LCD_CMD_FUNC_SET | LCD_8bit_MODE;
+  advanceState(EWC);
+  advanceState(eWC);
+
+  data_port->value = LCD_CMD_FUNC_SET | LCD_4bit_MODE;
+  advanceState(EWC);
+  advanceState(eWC);
+
+  data_port->value = LCD_CMD_FUNC_SET | LCD_4bit_MODE | LCD_2_LINES | LCD_SMALL_FONT;
+  advanceState(EWC);
+  advanceState(eWC);
+  data_port->value = (LCD_CMD_FUNC_SET | LCD_4bit_MODE | LCD_2_LINES | LCD_SMALL_FONT)<<4;
+  advanceState(EWC);
+  advanceState(eWC);
+
+
+  data_port->value = LCD_CMD_DISPLAY_CTRL | LCD_DISPLAY_ON; //LCD_CURSOR_OFF | LCD_BLINK_OFF
+  advanceState(EWC);
+  advanceState(eWC);
+  data_port->value = (LCD_CMD_DISPLAY_CTRL | LCD_DISPLAY_ON)<<4;
+  advanceState(EWC);
+  advanceState(eWC);
+   
+  data_port->value = LCD_CMD_CLEAR_DISPLAY;
+  advanceState(EWC);
+  advanceState(eWC);
+  data_port->value = LCD_CMD_CLEAR_DISPLAY << 4;
+  advanceState(EWC);
+  advanceState(eWC);
+
+  char *s ="ASHLEY & AMANDA";
+  int l = strlen(s);
+
+  for(int i=0; i<l; i++) {
+    data_port->value = s[i];
+    advanceState(EWD);
+    advanceState(eWD);
+    data_port->value = s[i] << 4;
+    advanceState(EWD);
+    advanceState(eWD);
+  }
+
+
+  viewInternals(0xff);
+}
