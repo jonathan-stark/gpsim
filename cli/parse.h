fa8b72b4da9b57360a7f380a70c9d2e246d8f45d
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2003-10-28 05:45:44 +0000

    Linked-list bug fixes in the new time stuff.

--- cli/parse.h
+++ cli/parse.h
@@ -1,3 +1,7 @@
+#ifndef BISON_PARSE_H
+# define BISON_PARSE_H
+
+#ifndef YYSTYPE
 typedef union {
   guint32              i;
   guint64             li;
@@ -7,44 +11,49 @@ typedef union {
   cmd_options_num   *con;
   cmd_options_float *cof;
   cmd_options_str   *cos;
-} YYSTYPE;
-#define	ABORT	257
-#define	ATTACH	258
-#define	BREAK	259
-#define	BUS	260
-#define	CLEAR	261
-#define	DISASSEMBLE	262
-#define	DUMP	263
-#define	FREQUENCY	264
-#define	HELP	265
-#define	LOAD	266
-#define	LOG	267
-#define	LIST	268
-#define	NODE	269
-#define	MODULE	270
-#define	PROCESSOR	271
-#define	QUIT	272
-#define	RESET	273
-#define	RUN	274
-#define	SET	275
-#define	STEP	276
-#define	STIMULUS	277
-#define	SYMBOL	278
-#define	TRACE	279
-#define	gpsim_VERSION	280
-#define	X	281
-#define	ICD	282
-#define	END_OF_COMMAND	283
-#define	IGNORED	284
-#define	SPANNING_LINES	285
-#define	STRING	286
-#define	INDIRECT	287
-#define	END_OF_INPUT	288
-#define	BIT_FLAG	289
-#define	NUMERIC_OPTION	290
-#define	STRING_OPTION	291
-#define	NUMBER	292
-#define	FLOAT_NUMBER	293
+} yystype;
+# define YYSTYPE yystype
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+# define	ABORT	257
+# define	ATTACH	258
+# define	BREAK	259
+# define	BUS	260
+# define	CLEAR	261
+# define	DISASSEMBLE	262
+# define	DUMP	263
+# define	FREQUENCY	264
+# define	HELP	265
+# define	LOAD	266
+# define	LOG	267
+# define	LIST	268
+# define	NODE	269
+# define	MODULE	270
+# define	PROCESSOR	271
+# define	QUIT	272
+# define	RESET	273
+# define	RUN	274
+# define	SET	275
+# define	STEP	276
+# define	STIMULUS	277
+# define	SYMBOL	278
+# define	TRACE	279
+# define	gpsim_VERSION	280
+# define	X	281
+# define	ICD	282
+# define	END_OF_COMMAND	283
+# define	IGNORED	284
+# define	SPANNING_LINES	285
+# define	STRING	286
+# define	INDIRECT	287
+# define	END_OF_INPUT	288
+# define	BIT_FLAG	289
+# define	NUMERIC_OPTION	290
+# define	STRING_OPTION	291
+# define	NUMBER	292
+# define	FLOAT_NUMBER	293
 
 
 extern YYSTYPE yylval;
+
+#endif /* not BISON_PARSE_H */
