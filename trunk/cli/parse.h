4ef6fbc1fde7ac71f4df15f9985dcaf7de4e81e5
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2004-02-18 05:52:52 +0000

    Scope resizing and expose now (kind of) work. There's a bash bug with regression scripts that I can't resolve.

--- trunk/cli/parse.h
+++ trunk/cli/parse.h
@@ -1,3 +1,7 @@
+#ifndef BISON_PARSE_H
+# define BISON_PARSE_H
+
+#ifndef YYSTYPE
 typedef union {
   guint32              i;
   guint64             li;
@@ -7,45 +11,50 @@ typedef union {
   cmd_options_num   *con;
   cmd_options_float *cof;
   cmd_options_str   *cos;
-} YYSTYPE;
-#define	ABORT	257
-#define	ATTACH	258
-#define	BREAK	259
-#define	BUS	260
-#define	CLEAR	261
-#define	DISASSEMBLE	262
-#define	DUMP	263
-#define	FREQUENCY	264
-#define	HELP	265
-#define	LOAD	266
-#define	LOG	267
-#define	LIST	268
-#define	NODE	269
-#define	MODULE	270
-#define	PROCESSOR	271
-#define	QUIT	272
-#define	RESET	273
-#define	RUN	274
-#define	SET	275
-#define	STEP	276
-#define	STIMULUS	277
-#define	STOPWATCH	278
-#define	SYMBOL	279
-#define	TRACE	280
-#define	gpsim_VERSION	281
-#define	X	282
-#define	ICD	283
-#define	END_OF_COMMAND	284
-#define	IGNORED	285
-#define	SPANNING_LINES	286
-#define	STRING	287
-#define	INDIRECT	288
-#define	END_OF_INPUT	289
-#define	BIT_FLAG	290
-#define	NUMERIC_OPTION	291
-#define	STRING_OPTION	292
-#define	NUMBER	293
-#define	FLOAT_NUMBER	294
+} yystype;
+# define YYSTYPE yystype
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+# define	ABORT	257
+# define	ATTACH	258
+# define	BREAK	259
+# define	BUS	260
+# define	CLEAR	261
+# define	DISASSEMBLE	262
+# define	DUMP	263
+# define	FREQUENCY	264
+# define	HELP	265
+# define	LOAD	266
+# define	LOG	267
+# define	LIST	268
+# define	NODE	269
+# define	MODULE	270
+# define	PROCESSOR	271
+# define	QUIT	272
+# define	RESET	273
+# define	RUN	274
+# define	SET	275
+# define	STEP	276
+# define	STIMULUS	277
+# define	STOPWATCH	278
+# define	SYMBOL	279
+# define	TRACE	280
+# define	gpsim_VERSION	281
+# define	X	282
+# define	ICD	283
+# define	END_OF_COMMAND	284
+# define	IGNORED	285
+# define	SPANNING_LINES	286
+# define	STRING	287
+# define	INDIRECT	288
+# define	END_OF_INPUT	289
+# define	BIT_FLAG	290
+# define	NUMERIC_OPTION	291
+# define	STRING_OPTION	292
+# define	NUMBER	293
+# define	FLOAT_NUMBER	294
 
 
 extern YYSTYPE yylval;
+
+#endif /* not BISON_PARSE_H */
--- trunk/regression/run_regression.sh
+++ trunk/regression/run_regression.sh
@@ -3,11 +3,11 @@
 # A meta-script that invokes each of the individual
 # regression tests.
 
-RT=rt.sh
+RT=./rt.sh
 # Instruction set simulation of the mid-range devices
 
 ${RT} instructions_14bit instructions_14bit
 
-${RT} node_test/node_test
+${RT} node_test node_test
 
-${RT} p16f84/p16f84
+# ${RT} p16f84 p16f84
