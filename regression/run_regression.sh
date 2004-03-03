dd31c61348a432041680eadceed8c26f50b2cba0
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2004-03-03 04:14:53 +0000

    Fixes to allow regression tests to run on BSD 4.9. patch from Bert Driehuis

--- regression/run_regression.sh
+++ regression/run_regression.sh
@@ -10,4 +10,11 @@ ${RT} instructions_14bit instructions_14bit
 
 ${RT} node_test node_test
 
-# ${RT} p16f84 p16f84
+#${RT} p12c509 it_12bit
+#${RT} p12c509 tmr0
+
+${RT} p16f84 p16f84
+
+#${RT} p16f628 p16f628
+
+#${RT} p16f873 p16f873
