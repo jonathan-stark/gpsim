6381bbbc5cc529f5d665d33180528c17bc483013
jrheisey <jrheisey@c0b0687f-f210-0410-a348-de8ada4243c3>
2005-07-24 21:49:16 +0000

    This batch file now copies a complete set of DLLs and support files necessary to run gpsim. It must be run from the gpsim/gpsim directory because it uses relative path for the source folders. You pass the target folder in as a parameter such as "Debug GUI" for Visual Studio.

--- gpsim/CopyDlls.bat
+++ gpsim/CopyDlls.bat
@@ -1,20 +1,35 @@
-rem This file was made to be run from the Visual Studio build
-echo off
+rem This file was made to be run from gpsim/gpsim directory.
+rem the only parameter would be the target directory.
+echo on
 set TargetDir=%1
 echo %0 %1
-if not exist %TargetDir%\popt1.dll copy ..\..\popt\bin\popt1.dll %TargetDir%
-if not exist %TargetDir%\libintl-2.dll copy ..\..\intl\bin\libintl-2.dll %TargetDir%
-if not exist %TargetDir%\libiconv-2.dll copy ..\..\iconv\bin\libiconv-2.dll %TargetDir%
-if not exist %TargetDir%\libglib-2.0-0.dll copy ..\..\glib\bin\libglib-2.0-0.dll %TargetDir%
-if not exist %TargetDir%\iconv.dll copy ..\..\iconv-1.9\bin\iconv.dll %TargetDir%
-if not exist %TargetDir%\intl.dll copy ..\..\gettext\bin\intl.dll %TargetDir%
-if not exist %TargetDir%\libgobject-2.0-0.dll copy ..\..\glib\bin\libgobject-2.0-0.dll %TargetDir%
-if not exist %TargetDir%\libgthread-2.0-0.dll copy ..\..\glib\bin\libgthread-2.0-0.dll %TargetDir%
-if not exist %TargetDir%\libgmodule-2.0-0.dll copy ..\..\glib\bin\libgmodule-2.0-0.dll %TargetDir%
-if not exist %TargetDir%\libgdk-win32-2.0-0.dll copy "..\..\gtk+\bin\libgdk-win32-2.0-0.dll" %TargetDir%
+if not exist %TargetDir%\bin\*.*                 md %TargetDir%\bin
+if not exist %TargetDir%\etc\*.*                 md %TargetDir%\etc
+if not exist %TargetDir%\lib\*.*                 md %TargetDir%\lib
+if not exist %TargetDir%\popt1.dll               copy ..\..\popt\bin\popt1.dll %TargetDir%
+if not exist %TargetDir%\libintl-2.dll           copy ..\..\intl\bin\libintl-2.dll %TargetDir%
+if not exist %TargetDir%\libiconv-2.dll          copy ..\..\iconv\bin\libiconv-2.dll %TargetDir%
+if not exist %TargetDir%\libglib-2.0-0.dll       copy ..\..\glib\bin\libglib-2.0-0.dll %TargetDir%
+if not exist %TargetDir%\iconv.dll               copy ..\..\iconv\bin\iconv.dll %TargetDir%
+if not exist %TargetDir%\intl.dll                copy ..\..\gettext\bin\intl.dll %TargetDir%
+if not exist %TargetDir%\libgobject-2.0-0.dll    copy ..\..\glib\bin\libgobject-2.0-0.dll %TargetDir%
+if not exist %TargetDir%\libgthread-2.0-0.dll    copy ..\..\glib\bin\libgthread-2.0-0.dll %TargetDir%
+if not exist %TargetDir%\libgmodule-2.0-0.dll    copy ..\..\glib\bin\libgmodule-2.0-0.dll %TargetDir%
+if not exist %TargetDir%\libgdk-win32-2.0-0.dll  copy "..\..\gtk+\bin\libgdk-win32-2.0-0.dll" %TargetDir%
 if not exist %TargetDir%\libgdk_pixbuf-2.0-0.dll copy "..\..\gtk+\bin\libgdk_pixbuf-2.0-0.dll" %TargetDir%
-if not exist %TargetDir%\libpango-1.0-0.dll copy ..\..\pango\bin\libpango-1.0-0.dll %TargetDir%
+if not exist %TargetDir%\libgtk-win32-2.0-0.dll  copy "..\..\gtk+\bin\libgtk-win32-2.0-0.dll" %TargetDir%
+if not exist %TargetDir%\etc\gtk-2.0\*.*         md %TargetDir%\etc\gtk-2.0
+if not exist %TargetDir%\etc\gtk-2.0\*.*         copy "..\..\gtk+\etc\gtk-2.0\*.*" %TargetDir%\etc\gtk-2.0
+if not exist %TargetDir%\lib\gtk-2.0\*.*         md %TargetDir%\lib\gtk-2.0
+if not exist %TargetDir%\lib\gtk-2.0\*.*         xcopy /s "..\..\gtk+\lib\gtk-2.0\*.*" %TargetDir%\lib\gtk-2.0
+
+if not exist %TargetDir%\libpango-1.0-0.dll      copy ..\..\pango\bin\libpango-1.0-0.dll %TargetDir%
 if not exist %TargetDir%\libpangowin32-1.0-0.dll copy ..\..\pango\bin\libpangowin32-1.0-0.dll %TargetDir%
-if not exist %TargetDir%\libgtk-win32-2.0-0.dll copy "..\..\gtk+\bin\libgtk-win32-2.0-0.dll" %TargetDir%
-if not exist %TargetDir%\libatk-1.0-0.dll copy "..\..\atk\bin\libatk-1.0-0.dll" %TargetDir%
+if not exist %TargetDir%\etc\pango\*.*           md %TargetDir%\etc\pango
+if not exist %TargetDir%\lib\pango\*.*           md %TargetDir%\lib\pango
+if not exist %TargetDir%\etc\pango\pango.*       xcopy /s ..\..\pango\etc\pango\*.* %TargetDir%\etc\pango
+if not exist %TargetDir%\lib\pango\pango.*       xcopy /s ..\..\pango\lib\pango\*.* %TargetDir%\lib\pango
+
+if not exist %TargetDir%\libatk-1.0-0.dll        copy "..\..\atk\bin\libatk-1.0-0.dll" %TargetDir%
+if not exist %TargetDir%\pthreadVC1.dll          copy "..\..\pthreads\lib\pthreadVC1.dll" %TargetDir%
 
