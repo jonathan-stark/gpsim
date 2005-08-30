fabd456d6e475b16b50dcfdd6fd39ac75a26a235
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2005-08-30 13:09:27 +0000

    Updated regression scripts to the new style of regression testing.

--- trunk/regression/rt.sh
+++ trunk/regression/rt.sh
@@ -1,25 +1,49 @@
-# test Script
+#!/bin/sh
 
-USAGE="Usage: `basename $0` DIRECTORY ASMFILE"
+# Regression Test Script for invoking specific targets in Makefiles
+#
+# 
+
+USAGE="Usage: `basename $0` DIRECTORY TARGET"
 
 if [ $# -lt 2 ] ; then
   echo "$USAGE"
   exit 1
 fi
 
-# this is where the test will be performed
+# this is the directory where the test will be performed
 cd $1
 
-# assemble
-gpasm  $2.asm
+# Create an start up script with the gpsim command
+#   load STC
+#
+# The makefiles invoke gpsim with the command
+#
+# gpsim -i -c startup.stc -D STC = "actual.stc"
+#
+# the -D option creates a symbol named STC with the string
+# value "actual.stc". The startup.stc file then symbolic
+# references the target specific simulation script via
+# the defined STC.
 
-# create the executable
 
-../create_stc $2 temp.stc
+STARTUP_STC='startup.stc'
 
-echo $2
+echo "load STC" > $STARTUP_STC
+echo "run" >> $STARTUP_STC
+echo "quit" >> $STARTUP_STC
+echo "" >> $STARTUP_STC
 
-../simulate temp.stc garbage.log
-cat garbage.log
-rm garbage.log
-rm temp.stc
+LOGFILE='simresults.log'
+touch $LOGFILE
+
+# Run the simulation and save the results
+make $2 >$LOGFILE
+
+# Convention simulations will print a message containing the
+# the word PASSED
+grep "PASSED" $LOGFILE
+
+if [ $? -ne 0 ] ; then
+  echo "$1/make $2 -- regression test FAILED"
+fi
