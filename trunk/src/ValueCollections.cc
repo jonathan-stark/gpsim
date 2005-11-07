6f03ef0e0ffd78d2166fb8598ab55ee90aca003d
jrheisey <jrheisey@c0b0687f-f210-0410-a348-de8ada4243c3>
2005-11-07 02:48:43 +0000

    * src/expr.cc src/expr.h, src/ValueCollections.cc,h, src/symbol.cc src/symbol.h: Implemented classes to expose to the command prompt interface an indexed variable symbol type commonly refered to as arrays.

---
+++ trunk/src/ValueCollections.cc
@@ -0,0 +1,113 @@
+/*
+   Copyright (C) 1998-2004 Scott Dattalo
+
+This file is part of gpsim.
+
+gpsim is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+gpsim is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gpsim; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#include "ValueCollections.h"
+#include "symbol.h"
+
+IIndexedCollection::IIndexedCollection(int iAddressRadix) {
+  SetAddressRadix(iAddressRadix);
+}
+
+void IIndexedCollection::SetAddressRadix(int iRadix) {
+  m_iAddressRadix = iRadix;
+  if(iRadix == 16) {
+    strcpy(m_szPrefix, "$");
+  }
+  else {
+    m_szPrefix[0] = 0;
+  }
+}
+
+void IIndexedCollection::SetAt(ExprList_t* pIndexers, Expression *pExpr) {
+  ExprList_t::iterator it;
+  ExprList_t::iterator itEnd = pIndexers->end();
+  Value * pValue = pExpr->evaluate();
+//    _CT * pCTValue = dynamic_cast<_CT*>(pValue);
+//    if(pCTValue != NULL) {
+    for(it = pIndexers->begin(); it != itEnd; it++) {
+      Value * pIndex = (*it)->evaluate();
+      Integer *pIntIndex = dynamic_cast<Integer*>(pIndex);
+      if(pIntIndex != NULL) {
+        SetAt(int(*pIntIndex), pValue);
+      }
+      if(pIndex != NULL) {
+        delete pIndex;
+      }
+    }
+//    }
+//    else {
+//    }
+  delete pValue;
+}
+
+char *IIndexedCollection::toString(char *pBuffer, int len) {
+  return strncpy(pBuffer, toString().c_str(), len);
+}
+
+string IIndexedCollection::toString() {
+  int iColumnWidth = 0;
+  vector<string> asIndexes;
+  vector<string> asValue;
+  ConsolidateValues(iColumnWidth, asIndexes, asValue);
+  return toString(iColumnWidth, asIndexes, asValue);
+}
+
+void IIndexedCollection::PushValue(int iFirstIndex, int iCurrentIndex,
+                                   Value *pValue,
+                                   vector<string> &asIndexes, vector<string> &asValue) {
+  ostringstream sIndex;
+  if(m_iAddressRadix == 16) {
+    sIndex << hex; 
+  }
+  sIndex << Value::name() << "[" << m_szPrefix << iFirstIndex;
+  if(iFirstIndex != (iCurrentIndex - 1)) {
+    sIndex << ".." << m_szPrefix << iCurrentIndex -1;
+  }
+  sIndex << "]" << '\000';
+  asIndexes.push_back(string(sIndex.str()));
+  asValue.push_back(pValue->toString());
+}
+
+string IIndexedCollection::toString(int iColumnWidth, vector<string> &asIndexes,
+                                    vector<string> &asValue) {
+  ostringstream sOut;
+  vector<string>::iterator itValue;
+  vector<string>::iterator itElement;
+  vector<string>::iterator itElementEnd = asIndexes.end();
+  // Dump the consolidated element list
+  for(itElement = asIndexes.begin(), itValue = asValue.begin();
+      itElement != itElementEnd;
+      itElement++, itValue++) {
+    sOut.width(iColumnWidth);
+    sOut.setf(ios_base::left);
+    sOut << (*itElement);
+    sOut << " = ";
+    sOut << (*itValue);
+    if(itElement + 1 != itElementEnd)
+      sOut << endl;
+  }
+  sOut << '\000';
+  return sOut.str();
+}
+
+Integer * IIndexedCollection::findInteger(const char *s) {
+  return get_symbol_table().findInteger(s);
+}
+
