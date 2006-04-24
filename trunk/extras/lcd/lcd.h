0c6195bb89aa4674b4f8280551b2335635d78ad0
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2006-04-24 04:25:54 +0000

    Rewrote the HD44780 driver model.

--- trunk/extras/lcd/lcd.h
+++ trunk/extras/lcd/lcd.h
@@ -33,7 +33,8 @@ Boston, MA 02111-1307, USA.  */
 #include <gtk/gtk.h>
 
 class LcdDisplay;
-
+class LCD_InputPin;
+class HD44780;
 
 // Create an interface to the simulator
 
@@ -58,88 +59,6 @@ public:
 };
 
 
-// Create a few classes from which an LCD may be constructed
-
-class Lcd_Port : public IOPORT
-{
-public:
-
-  LcdDisplay *lcd;
-  virtual void setbit(unsigned int bit_number, bool new_value);
-  virtual void assert_event(void);
-
-  Lcd_Port (LcdDisplay *_lcd,unsigned int _num_iopins=8);
-
-};
-
-class ControlPort : public Lcd_Port, public TriggerObject
-{
-public:
-
-  guint64 break_delta;
-  virtual void put(unsigned int new_value);
-  virtual void assert_event(void);
-  ControlPort (LcdDisplay *_lcd,unsigned int _num_iopins=8);
-  virtual void callback(void);
-};
-
-class DataPort : public Lcd_Port
-{
-public:
-  bool direction;
-  bool acceptingData;
-
-  virtual void put(unsigned int new_value);
-  virtual void assert_event(void);
-  virtual void setbit(unsigned int bit_number, bool new_value);
-  DataPort (LcdDisplay *_lcd,unsigned int _num_iopins=8);
-  void update_pin_directions(bool);
-
-};
-
-// Create a class derived from the IO_input class that
-// will allow us to intercept when the I/O input is being
-// driven. (This isn't done for PIC I/O pins because the
-// logic for handling I/O pin changes resides in the IOPORT
-// class.)
-
-class Lcd_Input : public IOPIN
-{
-public:
-
-  Lcd_Input (LcdDisplay *,IOPORT *i, unsigned int b, char *opt_name);
-
-};
-
-class Lcd_bi_directional : public IO_bi_directional
-{
-public:
-
-  Lcd_bi_directional(LcdDisplay *, IOPORT *i, unsigned int b,char *opt_name);
-
-};
-
-// LcdBusy - a class to handle the LCD's busy flag
-//
-// This class is essentially a timer. When the LCD requests that the
-// busy flag be set for a period of time, it will call the set() method
-// and pass the amount of time it wishes the flag to be busy. When
-// this time expires, the flag will get cleared.
-
-class LcdBusy : public TriggerObject
-{
- public:
-  LcdBusy(void) {bBusyState=false;}
-  void set(double waitTime);
-  void clear(void);
-  inline bool isBusy(void) { return bBusyState; }
-  virtual void callback(void);
-
- private:
-  bool bBusyState;
-
-};
-
 typedef char _5X7 [7][6];
 
 class LcdFont 
@@ -153,6 +72,7 @@ public:
 
   LcdFont(gint, GtkWidget *, LcdDisplay *);
   void update_pixmap(int, _5X7 *, LcdDisplay *);
+  GdkPixmap *getPixMap(unsigned int);
 };
 
 //------------------------------------------------------------------------
@@ -295,8 +215,14 @@ enum State {
   //_8BITMODE_START,
   //_8BITMODE,
 };
+enum ePins {
+  eDC,
+  eE,
+  eRW
+};
 
 
+//========================================================================
 class LcdDisplay : public Module
 {
 public:
@@ -312,12 +238,10 @@ public:
 
   State current_state, previous_state;
   ControlLineEvent last_event;
-  int mode_flag;
+  //  int mode_flag;
   int data_latch;
   int data_latch_phase;
   int debug;
-
-  LcdBusy busyTimer;
 
   SMEvent ControlEvents[8];
 
@@ -353,17 +277,7 @@ public:
   GdkColor
     *dot_color;     // LCD pixel color (controlled by contrast)
 
-  // Memory in a HD44780U is organized in 2x40 bytes of display RAM,
-  // and 64 bytes of character RAM
-#define CGRAM_ADDR_BITS 6
-#define CGRAM_SIZE (1 << CGRAM_ADDR_BITS)
-#define CGRAM_MASK (CGRAM_SIZE - 1)
-  gchar cgram_cursor;
-  gchar cgram[CGRAM_SIZE];
-  bool in_cgram;
-  bool cgram_updated;
-
-  gint **ch_data;
+  //gint **ch_data;
   gchar *title;
   GtkWidget *window;
   GtkWidget *darea;
@@ -407,26 +321,17 @@ public:
 
   GdkPixmap *get_pixmap(gint row, gint col);
 
-  void move_cursor(int new_row, int new_column);
+  void move_cursor(unsigned int new_row, unsigned int new_column);
   void clear_display(void);
   void write_data(int new_data);
-  void write_ddram_address(int data);
-  void write_cgram_address(int data);
 
-  // Here's the logic portion
-
-  ControlPort *control_port;
-  DataPort *data_port;
-
-  //  LCDStateMachine state_machine;
-
-  LcdDisplay(int aRows, int aCols, unsigned aType=0);
+  LcdDisplay(const char *, int aRows, int aCols, unsigned aType=0);
   ~LcdDisplay();
 
 
   void CreateGraphics (void);
-  void InitStateMachine(void);
-  void test(void);
+  //void InitStateMachine(void);
+  //void test(void);
   void build_window( void );
   void update(void);
   void update( GtkWidget *drawable,   
@@ -440,53 +345,26 @@ public:
   // Inheritance from Module class
   const virtual char *type(void) { return ("lcd_display"); };
   static Module *construct(const char *new_name);
+  void testHD44780();
+  bool dataBusDirection();
+  void UpdatePinState(ePins, char);
+protected:
 
+  LCD_InputPin *m_E;
+  LCD_InputPin *m_RW;
+  LCD_InputPin *m_DC;
+  PortRegister *m_dataBus;
 
-  // State Machine Functions:
-  void advanceState(ControlLineEvent e);
-  void newState(State s);
-  void revertState(void);
-  void viewInternals(int);   // debugging
+  HD44780 *m_hd44780;
+  unsigned int m_controlState;
+  bool cgram_updated;
 
-  char *getEventName( ControlLineEvent e);
-  char *getStateName( State s);
-
-  void execute_command(void);
-  void start_data(void);
-  void latch_data(void);
-  void send_status(void);
-  void read_data(void);
-  void release_port(void);
-  void new_command(void);
-  void new_data(void);
-
-  void set_8bit_mode(void) { mode_flag |= _8BIT_MODE_FLAG;};
-  void set_4bit_mode(void) { mode_flag &= ~_8BIT_MODE_FLAG;};
-  void set_2line_mode(void) { mode_flag |= _2LINE_MODE_FLAG;};
-  void set_1line_mode(void) { mode_flag &= ~_2LINE_MODE_FLAG;};
-  void set_large_font_mode(void) { mode_flag |= LARGE_FONT_MODE_FLAG;};
-  void set_small_font_mode(void) { mode_flag &= ~LARGE_FONT_MODE_FLAG;};
-  void set_display_on(void) { mode_flag |= DISPLAY_ON_FLAG;};
-  void set_display_off(void) { mode_flag &= ~DISPLAY_ON_FLAG;};
-  void set_blink_on(void) { mode_flag |= BLINK_ON_FLAG;};
-  void set_blink_off(void) { mode_flag &= ~BLINK_ON_FLAG;};
-  void set_cursor_on(void) { mode_flag |= CURSOR_ON_FLAG;};
-  void set_cursor_off(void) { mode_flag &= ~CURSOR_ON_FLAG;};
-
-  bool in_8bit_mode(void) {return ((mode_flag & _8BIT_MODE_FLAG) != 0);};
-  bool in_4bit_mode(void) {return ((mode_flag & _8BIT_MODE_FLAG) == 0);};
-  bool in_2line_mode(void) {return ((mode_flag & _2LINE_MODE_FLAG) != 0);};
-  bool in_1line_mode(void) {return ((mode_flag & _2LINE_MODE_FLAG) == 0);};
-  bool in_large_font_mode(void) {return ((mode_flag & LARGE_FONT_MODE_FLAG) != 0);};
-  bool in_small_font_mode(void) {return ((mode_flag & LARGE_FONT_MODE_FLAG) == 0);};
-  bool display_is_on(void) {return ((mode_flag & DISPLAY_ON_FLAG) != 0);};
-  bool display_is_off(void) {return ((mode_flag & DISPLAY_ON_FLAG) == 0);};
 };
 
 class LcdDisplayDisplaytech161A : public LcdDisplay
 {
 public:
-  LcdDisplayDisplaytech161A(int aRows, int aCols, unsigned aType);
+  LcdDisplayDisplaytech161A(const char *, int aRows, int aCols, unsigned aType);
   ~LcdDisplayDisplaytech161A();
 
   // Inheritance from Module class
