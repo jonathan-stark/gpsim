d1cf9a78383102bddb281536cbbb79fe7eab393f
sdattalo <sdattalo@c0b0687f-f210-0410-a348-de8ada4243c3>
2006-11-17 17:43:20 +0000

    Added. Supports the ClockPhase class, a base class designed for external clocking of processors and modules.

---
+++ trunk/src/clock_phase.cc
@@ -0,0 +1,66 @@
+/*
+   Copyright (C) 2006 T. Scott Dattalo
+
+This file is part of gpsim.
+
+gpsim is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+gpsim is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gpsim; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+
+#include "clock_phase.h"
+#if defined(CLOCK_EXPERIMENTS)
+
+#include "processor.h"
+#include "gpsim_time.h"
+
+//========================================================================
+ClockPhase::ClockPhase()
+{
+}
+
+ClockPhase::~ClockPhase()
+{
+}
+
+
+//========================================================================
+
+ProcessorPhase::ProcessorPhase(Processor *pcpu)
+  : ClockPhase(), 
+    m_pcpu(pcpu)
+{
+}
+ProcessorPhase::~ProcessorPhase()
+{
+}
+
+//========================================================================
+
+phaseExecute1Cycle::phaseExecute1Cycle(Processor *pcpu)
+  : ProcessorPhase(pcpu)
+{
+}
+phaseExecute1Cycle::~phaseExecute1Cycle()
+{
+}
+
+ClockPhase *phaseExecute1Cycle::advance()
+{
+  m_pcpu->step_one(false);
+  get_cycles().increment();
+  return this;
+}
+
+#endif // defined(CLOCK_EXPERIMENTS)
