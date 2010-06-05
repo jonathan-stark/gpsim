ca07488c0a2f295cac4ebfa430bb692e1821bdfc
roy_r_rankin <roy_r_rankin@c0b0687f-f210-0410-a348-de8ada4243c3>
2010-06-05 06:41:12 +0000

    LGPLv2+ license change

--- trunk/src/dspic/dspic-processors.cc
+++ trunk/src/dspic/dspic-processors.cc
@@ -1,22 +1,22 @@
 /*
    Copyright (C) 2006 T. Scott Dattalo
 
-This file is part of gpsim.
+This file is part of the libgpsim_dspic library of gpsim
 
-gpsim is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
 
-gpsim is distributed in the hope that it will be useful,
+This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with gpsim; see the file COPYING.  If not, write to
-the Free Software Foundation, 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, see 
+<http://www.gnu.org/licenses/lgpl-2.1.html>.
+*/
 
 
 #include "dspic-processors.h"
